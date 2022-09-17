f3b41efc37a4d92dd6dd819aa96f31f8c584fd1c
leonardomunoz <l.mnz.lpz@gmail.com>
2022-09-17 01:31:28 -0600

    config page UI 90% done

---
+++ Project/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='Blue-rectangle'></header>
+      <header className="App-header">
+        Welcome!
+      </header>
+      <form className='Import-project-content-holder'>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <div className='Gray-seperator'></div>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <div className='Gray-seperator'></div>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button id="importProjectTxtBox" type="text"> Import Project</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
