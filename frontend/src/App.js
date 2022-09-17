7e87a3b76fc84c924a5e815b11cfffedecab7e44
lmnzlpz <60372910+lmnzlpz@users.noreply.github.com>
2022-09-17 16:56:52 -0600

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+const avoidSubmit = event => {
+  event.preventDefault()
+  console.log('You clicked the button')
+}
 
 function App() {
   return (
     <div className="App">
+      <header className='Blue-rectangle'></header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ahoy Mateys
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome!
       </header>
+      <form className='Import-project-content-holder'>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importProjectButton"type="text"> Import Project</button>
+        <div className='Gray-seperator'></div>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="openProjectButton" type="text"> Open Project</button>
+        <div className='Gray-seperator'></div>
+        <input className="configInputBoxes" id="userInitialTextBox" type="text" placeholder='User Initials'/> 
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importBlacklistButton" type="text"> Import Blacklist(s)</button>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importDBCButton" type="text"> Import DBC File</button>
+        <input className="configInputBoxes" id="eventNameTxtBox" type="text" placeholder='Event Name'/>
+        <input className="configInputBoxes" id="setBaudTxtBox"type="text" placeholder="Set Baud Rate" />
+        <button className='doneButton'>Done</button>
+
+      </form>
     </div>
   );
 }
