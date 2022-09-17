067eb890a5c8d739547db133f38c15e2f8ce24b7
leonardomunoz <l.mnz.lpz@gmail.com>
2022-09-17 16:46:02 -0600

    Working Config Page UI

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 
+const avoidSubmit = event => {
+  event.preventDefault()
+  console.log('You clicked the button')
+}
+
 function App() {
   return (
     <div className="App">
@@ -8,15 +13,16 @@ function App() {
         Welcome!
       </header>
       <form className='Import-project-content-holder'>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importProjectButton"type="text"> Import Project</button>
         <div className='Gray-seperator'></div>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="openProjectButton" type="text"> Open Project</button>
         <div className='Gray-seperator'></div>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
-        <button id="importProjectTxtBox" type="text"> Import Project</button>
+        <input className="configInputBoxes" id="userInitialTextBox" type="text" placeholder='User Initials'/> 
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importBlacklistButton" type="text"> Import Blacklist(s)</button>
+        <button onClick={avoidSubmit} className="configInputBoxes" id="importDBCButton" type="text"> Import DBC File</button>
+        <input className="configInputBoxes" id="eventNameTxtBox" type="text" placeholder='Event Name'/>
+        <input className="configInputBoxes" id="setBaudTxtBox"type="text" placeholder="Set Baud Rate" />
+        <button className='doneButton'>Done</button>
 
       </form>
     </div>
