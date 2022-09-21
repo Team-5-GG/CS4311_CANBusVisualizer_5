165c37750ea5183fa2a658b39bb683034c7d0b70
leonardomunoz <l.mnz.lpz@gmail.com>
2022-09-21 09:50:17 -0600

    using local storage for string storage. File upload not working yet.

--- frontend/src/introConfigPage.js
+++ frontend/src/introConfigPage.js
@@ -5,6 +5,28 @@ function reroute() {
   window.location.href = './edit'
 };
 
+function saveData(){
+  let userinitials = document.getElementById("userInitialTextBox").value;
+  let eventName = document.getElementById("eventNameTxtBox").value;
+  let baudRate = document.getElementById("setBaudTxtBox").value;
+
+  let newProjectData = new configDataHolder(userinitials, eventName, baudRate);
+  let newProjectDataString = JSON.stringify(newProjectData);
+
+  localStorage.setItem("data", newProjectDataString);
+  
+}
+
+class configDataHolder{
+  constructor(userinitials, eventName, setBaudRate){
+    this.userinitials = userinitials;
+    this.eventName = eventName;
+    this.setBaudRate = setBaudRate;
+  }
+};
+
+
+
 
 
 
@@ -16,7 +38,10 @@ function IntroConfigPage() {
         Welcome!
       </header>
       <form className='Import-project-content-holder'>
-        <button type="button" className="configInputBoxes" id="importProjectButton"> Import Project</button>
+        <button type="button" className="configInputBoxes" id="importProjectButton">Import Project</button> 
+          
+        
+        
         <div className='Gray-seperator'></div>
         <button type="button" className="configInputBoxes" id="openProjectButton"> Open Project</button>
         <div className='Gray-seperator'></div>
@@ -25,7 +50,7 @@ function IntroConfigPage() {
         <button type="button" className="configInputBoxes" id="importDBCButton"> Import DBC File</button>
         <input className="configInputBoxes" id="eventNameTxtBox" type="text" placeholder='Event Name'/>
         <input className="configInputBoxes" id="setBaudTxtBox"type="text" placeholder="Set Baud Rate" />
-        <button type="button" onClick={reroute} className='doneButton'>Done</button>
+        <button type="button" onClick={saveData} className='doneButton'>Done</button>
         
 
       </form>      
