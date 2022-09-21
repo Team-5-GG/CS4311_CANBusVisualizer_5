45ec0363e657e3ae32d12d5cee5f265552760c9f
mjones97jc <mjones97jc@gmail.com>
2022-09-21 12:04:59 -0600

    merged changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:frontend/src/introConfigPage.js
-import './buttonsAndTxtBxs.css';
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom"
->>>>>>> cd4c41becc890e257a0fafde7eb41daf23a721c3:frontend/src/App.js
 
 //pages & components
 import Home from "./pages/Home";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import './buttonsAndTxtBxs.css';
-import App from './introConfigPage.js';
-import reportWebVitals from './reportWebVitals';
-=======
 import './index.css';
 import App from './App';
->>>>>>> cd4c41becc890e257a0fafde7eb41daf23a721c3
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
