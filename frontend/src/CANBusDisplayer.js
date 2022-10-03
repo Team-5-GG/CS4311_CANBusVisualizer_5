db73d5a6fc951d1564c2d81e61a02ff1dfb738a0
leonardomunoz <l.mnz.lpz@gmail.com>
2022-10-03 16:06:46 -0600

    The CAN Bus map displayer is scrollable

--- frontend/src/CANBusDisplayer.js
+++ frontend/src/CANBusDisplayer.js
@@ -3,7 +3,9 @@ import './displayer.css'
 export function CANBusDisplayer (){
     return(
         <div className="CANBusMapContainer">
-            
+            <div className="overFlowContent">
+                <div className='box'></div>
+            </div>
         </div>
     );
 }
