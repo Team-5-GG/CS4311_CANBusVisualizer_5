f5d2c4326fc600ec38f8d8b3f41d178d50b9efc5
Cordero <amcordero4@miners.utep.edu>
2022-10-03 16:42:02 -0600

    fixing popup

--- frontend/src/components/createPopup.js
+++ frontend/src/components/createPopup.js
@@ -5,20 +5,19 @@ function Modal({closeModal}){
         <div className="modal-background">
             <div className="modal-container">
                 <button id = "closeCreateWindow" onClick={() => closeModal(false)}> X </button>
-                <div className="title">
+                <div className="createProject-title">
                     <h1> Create Project</h1>
                 </div>
                 <div className="modal-body">
-                    <form>
-                        <input className="configInputBoxes" id="userInitialTextBox" type="text" placeholder='User Initials'/> 
-                        <button type="button" className="configInputBoxes" id="importBlacklistButton"> Import Blacklist(s)</button>
-                        <button type="button" className="configInputBoxes" id="importDBCButton"> Import DBC File</button>
-                        <input className="configInputBoxes" id="eventNameTxtBox" type="text" placeholder='Event Name'/>
-                        <input className="configInputBoxes" id="setBaudTxtBox"type="text" placeholder="Set Baud Rate" />
+                    <form className="createProjectTextBox">
+                        <input className="configInputBoxes" id="createProjectTextBox" type="text" placeholder='User Initials'/> 
+                        <button type="button" className="configInputBoxes" id="createProjectTextBox"> Import Blacklist(s)</button>
+                        <button type="button" className="configInputBoxes" id="createProjectTextBox"> Import DBC File</button>
+                        <input className="configInputBoxes" id="createProjectTextBox" type="text" placeholder='Event Name'/>
+                        <input className="configInputBoxes" id="createProjectTextBox"type="text" placeholder="Set Baud Rate" />
                     </form>
                 </div>
-                <div className="footer">
-                    <button>Close</button>
+                <div className="createProject-footer">
                     <button>Done</button>
                 </div>
             </div>
--- frontend/src/introConfigPage.js
+++ frontend/src/introConfigPage.js
@@ -39,22 +39,6 @@ function IntroConfigPage() {
   const [openModal, setOpenModal] = useState(false);
   return (
     <div className="App">
-      {/* Tab buttons */}
-      <div className='Tab-buttons'>
-        <button type="button" className="min">
-          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-window-dash" viewBox="0 0 16 16">
-            <path d="M2.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1ZM4 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1Zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0Z"/>
-            <path d="M0 4a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v4a.5.5 0 0 1-1 0V7H1v5a1 1 0 0 0 1 1h5.5a.5.5 0 0 1 0 1H2a2 2 0 0 1-2-2V4Zm1 2h13V4a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2Z"/>
-            <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-5.5 0a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5Z"/>
-          </svg>
-        </button> 
-        <button type="button" className="close">
-          <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="white" class="bi bi-x-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-          </svg>
-        </button> 
-      </div>
       {/* Page title */}
       <header className="App-header">
         CAN Bus Visualizer
