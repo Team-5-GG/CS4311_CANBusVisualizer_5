1651aea2bfc70c633c55dcba880232c84a76f222
leonardomunoz <l.mnz.lpz@gmail.com>
2022-10-16 17:40:36 -0600

    Made the Bus node be in the middle and be unmovable by user

--- frontend/src/CANBusDisplayer.js
+++ frontend/src/CANBusDisplayer.js
@@ -38,11 +38,18 @@ function initDiagram() {
     // define a simple Node template
     diagram.nodeTemplate =
       $(go.Node, 'Auto',  // the Shape will go around the TextBlock
+        new go.Binding("selectable",'selec'),
+        new go.Binding("pickable", "pick"),
         new go.Binding('location', 'loc', go.Point.parse).makeTwoWay(go.Point.stringify),
-        $(go.Shape, 'RoundedRectangle',
-          { name: 'SHAPE', fill: 'white', strokeWidth: 0, portId:"", fromLinkable:true, toLinkable:true },
+        $(go.Shape,
+          { name: 'SHAPE', strokeWidth: 3, portId:"", fromLinkable:true, toLinkable:true},
           // Shape.fill is bound to Node.data.color
-          new go.Binding('fill', 'color')), // permettre de lire la color et fill
+          new go.Binding("toLinkable", "to"),
+          new go.Binding('fill', 'color'),
+          new go.Binding("figure", 'figure'),
+          new go.Binding('width', 'width')
+          ), 
+        //   {selectable:(go.Shape.figure == "LineH" ? true : false)}, // permettre de lire la color et fill
         $(go.TextBlock,
           { margin: 8, editable: true },  // some room around the text
           new go.Binding('text').makeTwoWay() //permettre de lire le texte
@@ -54,42 +61,23 @@ function initDiagram() {
                 $(go.Shape),
                 $(go.Shape, {toArrow: "Standard"})
             );
-    // diagram.linkTemplateMap.add("linkToLink", 
-    //     $("Link",
-    //     {relinkableFrom:true, relinkableTo:true},
-    //     $("Shape", {stroke: "#2D9945", strokeWidth: 2 })));
-
-    // diagram.model = new go.GraphLinksModel(
-    //     {linkLabelKeysProperty: "labelKeys"}
-    // );
-    // diagram.toolManager.linkingTool.archetypeLabelNodeData =
-    //     { category: "LinkLabel" };
-
-    //     function maybeChangeLinkCategory(e) {
-    //         var link = e.subject;
-    //         var linktolink = (link.fromNode.isLinkLabel || link.toNode.isLinkLabel);
-    //         e.diagram.model.setCategoryForLinkData(link.data, (linktolink ? "linkToLink" : ""));
-    //       }
-
-    console.log(test.nodeDataArray);
     return diagram;
   }
 
+  
+
   function handleModelChange(changes) {
     console.log('changes', changes)
+
   }
 
 
 export function CANBusDisplayer (){
+   
   
     return(
-        // <MyDiagram id="myDiagramDiv" className="CANBusMapContainer">
-        //     {/* <div className="overFlowContent">
-        //         <div className='box'></div>
-        //     </div> */}
-        // </MyDiagram>
         <div>
-  <ReactDiagram
+    <ReactDiagram
         initDiagram={initDiagram}
         divClassName='diagram-component'
         nodeDataArray= {test.nodeDataArray}
