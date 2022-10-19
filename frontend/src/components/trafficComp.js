a11f6670b8c850b09d875d4a3e084c39fa593224
kmacosta2 <kmacosta2@miners.utep.edu>
2022-10-19 12:08:01 -0600

    lazy loading traffic

---
+++ frontend/src/components/trafficComp.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from "react";
+import data from "../fakeTraffic-data.json";
+//import SyncPopup from './components/syncProjectPopup';
+
+
+
+export default () => {
+    const [packets, setPackets] = useState(data);
+    return (<tbody> 
+        {packets.map((packet) => (
+            <tr className="packetRow">
+                <td className="packetId">{packet.id}</td>
+                <td>{packet.source}</td>
+                <td>{packet.destination}</td>
+                <td>{packet.rawData}</td>
+                <td>{packet.Description}</td>
+            </tr>
+         ))}
+    </tbody>)
+}
+
+
