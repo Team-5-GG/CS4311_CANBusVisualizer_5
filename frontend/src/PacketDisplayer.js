c83ed7e0f6306e88ac086d73040c505b997a8cc7
leonardomunoz <l.mnz.lpz@gmail.com>
2022-11-29 19:31:16 -0500

    f

--- frontend/src/PacketDisplayer.js
+++ frontend/src/PacketDisplayer.js
@@ -5,16 +5,21 @@ import { useState } from "react";
 import $ from 'jquery'; 
 //import data from "./fakeTraffic-data.json";
 import 'datatables.net';
+import 'datatables.net-buttons-bs5';
 
 
 
 export function PacketDisplayer() {
-    $(document).ready(function() {
+    let table = $(document).ready(function() {
         $("#dtHorizontalVerticalExample").DataTable({
             retrieve: true,
-            select:true
+            select:true,
+            buttons:[
+                'Edit', 'Replay', 'Delete'
+            ]
         });
         });
+    table.buttons().container().appendTo($('.col-sm-6:eq(0)', table.table().container()));
 
     
 
@@ -35,7 +40,6 @@ export function PacketDisplayer() {
                 </thead>
                 <Traffic />
             </table>
-            <ContextMenu></ContextMenu>
         </div>
     );
 }
