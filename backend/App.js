8258cf2c50242fc92a8caf65f4c1076cdc590c41
andreapaola2018 <apulloa@miners.utep.edu>
2022-12-01 05:15:27 +0300

    backend updates

--- backend/App.js
+++ backend/App.js
@@ -1,23 +1,76 @@
+// import TrafficHolder from "./traffic.js";
+// import Channel from "./channel.js";
+// import NodeHolder from "./nodelist.js";
+
+// export var traffic;
+// export var channel;
+// export var nodeHolder;
+
+// export default function runApp(app){
+//     traffic = new TrafficHolder();
+//     nodeHolder = new NodeHolder();
+
+//     channel = new Channel('can0', 100, traffic, nodeHolder);
+
+//     //channel.start()
+//     //var timer = setInterval(()=>{console.log('stopping'); channel.stop(); clearInterval(timer);}, 3000)
+//     //var timer2 = setInterval(()=>{console.log('starting'); channel.start(); clearInterval(timer2);}, 6000)
+    
+//     // while (true){
+//     //     channel.send('392', Buffer.from([0x01, 0x00, 0x00, 0x00]));
+//     // }
+//     //return JSON.stringify({packet: traffic.traffic[traffic.traffic.length-1]});
+// }
+
 import TrafficHolder from "./traffic.js";
 import Channel from "./channel.js";
-import NodeHolder from "./nodelist.js";
+// import NodeHolder from "./nodelist.js";
 
-export var traffic;
-export var channel;
-export var nodeHolder;
+export var nodeHolder
+export var traffic
+var channel
 
-export default function runApp(app){
+export function runApp(){
     traffic = new TrafficHolder();
-    nodeHolder = new NodeHolder();
-
-    channel = new Channel('can0', 100, traffic, nodeHolder);
-
-    //channel.start()
-    //var timer = setInterval(()=>{console.log('stopping'); channel.stop(); clearInterval(timer);}, 3000)
-    //var timer2 = setInterval(()=>{console.log('starting'); channel.start(); clearInterval(timer2);}, 6000)
     
+    channel = new Channel(traffic);
+
+    channel.start();
+
+    // var timer = setInterval(function(){
+    //     channel.stop();
+    //     clearInterval(timer);
+
+    //     let timeRange = [new Date('2022-11-26T21:25:00.000Z'),new Date('2022-11-26T21:26:00.000Z')]
+        
+    //     // timeRange = JSON.stringify(timeRange)
+
+    //     // console.log(JSON.parse(timeRange))
+
+    //     console.log(traffic.traffic)
+
+    //     console.log(traffic.traffic[0].timestamp)
+
+    //     console.log(new Date('2022-11-26T21:03:00.000Z'))
+
+    //     console.log('BEGINNING FILTERING...')
+
+    //     timeRange = ["2022-11-29T03:22:50.000Z", "2022-11-29T03:23:51.000Z"]
+
+    //     let filteredPackets = traffic.filterPackets(null, 'UPBAC', null)
+
+    //     console.log(filteredPackets)
+    // }, 3000)
+
     // while (true){
     //     channel.send('392', Buffer.from([0x01, 0x00, 0x00, 0x00]));
     // }
-    //return JSON.stringify({packet: traffic.traffic[traffic.traffic.length-1]});
+}
+
+export function stopApp(){
+    console.log('Releasing variables...')
+    traffic = undefined
+
+    console.log('Closing channel...')
+    channel.stop()
 }
--- backend/traffic.js
+++ backend/traffic.js
@@ -17,6 +17,36 @@ export default class TrafficHolder{
         this.#traffic.push(packet)
     }
 
+     // filter by time, node, and packet size
+     filterPackets(time, node, size){
+        let filterList = []
+        let copyList = this.#traffic.slice()
+
+        if(time){
+            let beg = new Date(time[0])
+            let end = new Date(time[1])
+
+            filterList.push(...copyList.filter(packet => packet.timestamp.getTime() >= beg.getTime() && packet.timestamp.getTime() <= end.getTime()))
+
+            // take the copied list and filter all other packets that did not match condition
+            // so as to possibly get them in the later filters (and prevent duplicates)
+            copyList = copyList.filter(packet => !(packet.timestamp.getTime() >= beg.getTime() && packet.timestamp.getTime() <= end.getTime()))
+        }
+
+        if(node){
+            filterList.push(...copyList.filter(packet => packet.name == node))
+            copyList = copyList.filter(packet => !(packet.name == node))
+        }
+
+        if(size){
+            filterList.push(...copyList.filter(packet => packet.rawPacket.data.length == size))
+            copyList = copyList.filter(packet => !(packet.rawPacket.data.length == size))
+        }
+
+        // THROW ERROR IF NO CONDITIONS ARE MET FOR FILTER I.E. FILTERLIST IS EMPTY
+        return filterList
+    }
+
     // filter by time, node, and packet size
     filter(){
 
